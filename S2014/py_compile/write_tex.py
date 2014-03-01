d18aa4fd30a01a46b770e5220323ca61cb628303
Nils Olsson <nils.john.olsson@gmail.com>
2014-03-01 14:55:51 -0800

    Beginning work on py script to build into latex for better automization

---
+++ S2014/py_compile/write_tex.py
@@ -0,0 +1,33 @@
+import os
+
+# Get list of all pytex files
+def GetPyTeXList():
+    pytex_files = []
+    for root, dirs, files in os.walk('.'):
+        for f in files:
+            if f.endswith('.pytex'):
+                pytex_files.append(os.path.join(root,f))
+    return pytex_files
+
+# Get selections from list of files
+def GetPyTeXSelections(files):
+    if not files:
+        print('Error: No PyTeX documents in directory tree.')
+    else:
+        for i in range(0, len(files)):
+            print('[%s]: "%s"'%(i, files[i]))
+
+# Open specified file
+# TODO PyTeX format specific file operations
+def OpenDocument(path_rawfile):
+    return open(path_rawfile, 'r')
+
+def BeginDocument(file, *pkgs):
+    with file as f:
+        print(file.line)
+
+def Main():
+    pytex_files = GetPyTeXList()
+    GetPyTeXSelections(pytex_files)
+
+Main()
